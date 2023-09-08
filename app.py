eea0b3feafa0db243efc32ff008a09f76b7f1523
Kiplangat Stanley <39749228+jythonkip@users.noreply.github.com>
2023-09-08 17:18:59 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def get_info():
+    # Get query parameters
+    slack_name = request.args.get('slack_name')
+    track = request.args.get('track')
+
+    # Get current day of the week
+    current_day = datetime.now(pytz.utc).astimezone(pytz.timezone('UTC')).strftime('%A')
+
+    # Get current UTC time
+    current_utc_time = datetime.now(pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # GitHub URLs
+    github_file_url = "https://github.com/jython/hngx-backend/blob/main/file_name.ext"
+    github_repo_url = "https://github.com/jython/hngx-backend"
+
+    # Create the JSON response
+    response_data = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": current_utc_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
